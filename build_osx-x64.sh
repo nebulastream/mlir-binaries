dc0e1dbe4ba9ab4c091c724f2fc5633128759ee2
Philipp Grulich <philippgrulich@hotmail.de>
2023-01-07 18:48:30 +0100

    disable clang z3

--- build_osx-x64.sh
+++ build_osx-x64.sh
@@ -31,6 +31,7 @@ cmake -G Ninja -S llvm -B build -DCMAKE_BUILD_TYPE=Release \
 				-DLLVM_ENABLE_TERMINFO=OFF \
 				-DLLVM_ENABLE_EH=ON\
 				-DLLVM_ENABLE_RTTI=ON\
+				-DLLVM_ENABLE_Z3_SOLVER=OFF \
                 -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" \
 				-DCOMPILER_RT_ENABLE_IOS=OFF \
 				-DCOMPILER_RT_ENABLE_WATCHOS=OFF \
--- build_ubuntu-x64.sh
+++ build_ubuntu-x64.sh
@@ -29,8 +29,9 @@ cmake -G Ninja -S llvm -B build -DCMAKE_BUILD_TYPE=Release \
 				-DLLVM_TARGETS_TO_BUILD="X86" \
 				-DLLVM_BUILD_TOOLS=OFF \
 				-DLLVM_ENABLE_TERMINFO=OFF \
-				-DLLVM_ENABLE_EH=ON\
-				-DLLVM_ENABLE_RTTI=ON\
+				-DLLVM_ENABLE_EH=OFF \
+				-DLLVM_ENABLE_RTTI=OFF \
+				-DLLVM_ENABLE_Z3_SOLVER=OFF \
                 -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt" 
 ninja -C build 
 ninja -C build clang-format
